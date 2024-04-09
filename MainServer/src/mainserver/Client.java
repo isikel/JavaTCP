18f626264975f6671c0e0fd3cc4198fd17a50a5b
isikel <152266713+isikel@users.noreply.github.com>
2024-04-09 10:38:55 +0200

    Update Client.java

--- MainServer/src/mainserver/Client.java
+++ MainServer/src/mainserver/Client.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mainserver;
-
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 /**
  *
  * @author Studente
  */
 public class Client {
+    String nome;
+    String colore;
+    Socket s;
     
+    Client (String nomeDefault, String coloreDefault){
+        nome = nomeDefault;
+        colore = coloreDefault;
+    }
+    
+    public void connetti(String nomeServer, int portaServer){
+        try{
+           s = new Socket(nomeServer, portaServer);
+        }catch(UnknownHostException ue){
+            System.err.println("Errore DNS");
+        }catch(IOException ex){
+            System.err.println("errore");
+        }
+    }
+    
+    public void scrivi(){
+        
+    }
+    
+    public void leggi(){
+        
+    }
+    
+    public void chiudi(){
+        try{
+           s.close();
+           System.out.println("chiusura connessione");
+        }catch(IOException ex){
+            System.err.println("errore chiusura connessione");
+        }
+    }
 }
